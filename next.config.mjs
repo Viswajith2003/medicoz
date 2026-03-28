de088cb68d2012b846422455c1089df3e94e81cc
Viswajith2003 <viswajithviswa715@gmail.com>
2026-03-28 06:27:14 +0530

    Fixed Vercel AWS EC2 mixed content block

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://3.27.1.180:7000/:path*',
+      },
+    ]
+  },
+};
 
 export default nextConfig;
